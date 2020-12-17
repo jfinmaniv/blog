1a09e603e5e84ee350692a6b5512234ef3489232
John Inman <jfinmaniv@icloud.com>
2020-12-17 10:45:24 -0800

    rebuilding site

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,11 @@ set -e
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
+# commit project folder
+git add -u
+git commit -m 'rebuilding site'
+git push origin master
+
 # Build the project.
 hugo "$@" # if using a theme, replace with `hugo -t <YOURTHEME>`
 
